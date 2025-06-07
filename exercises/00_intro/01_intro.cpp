4ca1e4b7db47ebb2326d64492b1fcb1cc6b07198
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-06-07 06:26:22 -0400

    feat: create splash screen

---
+++ exercises/00_intro/01_intro.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void splashScreen()
+{
+
+    // user input
+    char userInput{};
+
+    // splash screen
+    std::cout << "          Welcome to cpplings!        \n";
+    std::cout << "                  _ _                 \n";
+    std::cout << "                 | (_)                \n";
+    std::cout << "  ___ _ __  _ __ | |_ _ __   __ _ ___ \n";
+    std::cout << " / __| '_ \\| '_ \\| | | '_ \\ / _` / __|\n";
+    std::cout << "| (__| |_) | |_) | | | | | | (_| \\__ \\\n";
+    std::cout << " \\___| .__/| .__/|_|_|_| |_|\\__, |___/\n";
+    std::cout << "     | |   | |               __/ |    \n";
+    std::cout << "     |_|   |_|              |___/     \n";
+    std::cout << "                                      \n";
+    std::cout << "          Enter Y/y to continue       \n";
+
+    while (true)
+    {
+        std::cin >> userInput;
+
+        if (userInput == 'y' || userInput == 'Y')
+        {
+            break;
+        }
+    }
+}
