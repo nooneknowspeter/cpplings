f4623fbb0e440e77b932c6e48ef1dac0d26b3869
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-11 12:18:01 -0400

    feat: add 01_variables.cpp

---
+++ exercises/01_variables/01_variables.cpp
@@ -0,0 +1,26 @@
+// Initializing and assignment of variables in C++ is as follows:
+// datatype variableName{value};
+//
+// or
+//
+// datatype variableName = value;
+//
+// Using the first example is called Direct List Initialization.
+// It is a modern way of intializing variables in C++
+//
+// The second method is a classic way of initializing and assigning
+// values to variables.
+//
+// https://www.learncpp.com/cpp-tutorial/introduction-to-objects-and-variables/
+// https://www.learncpp.com/cpp-tutorial/variable-assignment-and-initialization/
+
+// TODO: Add the missing module for printing
+#include <iostream>
+
+void variables01()
+{
+    // TODO: Add the missing keyword
+    int x{5};
+
+    std::cout << x;
+}
