e58e9811c4f64a8daeaeed70104143977c079268
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-11 12:19:49 -0400

    fix: disable continue prompt in splash-screen

--- exercises/00_intro/01_intro.cpp
+++ exercises/00_intro/01_intro.cpp
@@ -3,11 +3,11 @@
 void splashScreen()
 {
 
-    // user input
-    char userInput{};
+    // // user input
+    // char userInput{};
 
     // splash screen
-    std::cout << "          Welcome to cpplings!        \n";
+    // std::cout << "          Welcome to cpplings!        \n";
     std::cout << "                  _ _                 \n";
     std::cout << "                 | (_)                \n";
     std::cout << "  ___ _ __  _ __ | |_ _ __   __ _ ___ \n";
@@ -17,15 +17,15 @@ void splashScreen()
     std::cout << "     | |   | |               __/ |    \n";
     std::cout << "     |_|   |_|              |___/     \n";
     std::cout << "                                      \n";
-    std::cout << "          Enter Y/y to continue       \n";
+    // std::cout << "          Enter Y/y to continue       \n";
 
-    while (true)
-    {
-        std::cin >> userInput;
-
-        if (userInput == 'y' || userInput == 'Y')
-        {
-            break;
-        }
-    }
+    // while (true)
+    // {
+    //     std::cin >> userInput;
+    //
+    //     if (userInput == 'y' || userInput == 'Y')
+    //     {
+    //         break;
+    //     }
+    // }
 }
