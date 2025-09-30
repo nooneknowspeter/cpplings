3b031dc15a0fd10ea562aa35c388a507fb1394a5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-09-30 04:25:10 -0400

    chore: update ssh agent script

--- scripts/ssh-add-key.sh
+++ scripts/ssh-add-key.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-eval "$(ssh-agent)"
-ssh-add "$1"
+if [[ "$1" != "" ]]; then
+  eval "$(ssh-agent)"
+  ssh-add "$1"
+fi
+
+echo -e "pass your private ssh-key as an argument:
+\n
+./ssh-add-key.sh ~/.ssh/id_ed25519"
+
