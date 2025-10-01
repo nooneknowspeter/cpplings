5705ba8c411ac988ba820844eadadebd5ddd7703
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-10-01 12:52:05 -0400

    feat(build): automate compile flags generation
    - dynamically add header paths

--- build.zig
+++ build.zig
@@ -71,15 +71,24 @@ pub fn build(b: *STD.Build) !void {
     cflags.addIncludePath(b.path("src"));
     cflags.addIncludePath(DEP_GTEST.path("include"));
 
-    // TODO: automate libcc path
-    // $ clang++ -E -x c++ - -v < /dev/null
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/a3c2pnnyycikxs9gnxgakvilajyxhyv2-lldb-19.1.7-dev/include" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/include" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/n7p5cdg3d55fr659qm8h0vynl3rcf26h-compiler-rt-libc-19.1.7-dev/include" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/include/c++/14.3.0" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/include/c++/14.3.0/x86_64-unknown-linux-gnu" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/fbfcll570w9vimfbh41f9b4rrwnp33f3-clang-wrapper-19.1.7/resource-root/include" });
-    cflags.addIncludePath(.{ .cwd_relative = "/nix/store/gf3wh0x0rzb1dkx0wx1jvmipydwfzzd5-glibc-2.40-66-dev/include" });
+    const CLANG_PLUS_PLUS = try STD.process.Child.run(.{ .allocator = b.allocator, .argv = &[_][]const u8{ "zig", "c++", "-E", "-x", "c++", "-", "-v" } });
+    var clang_plus_plus_output = STD.mem.splitScalar(u8, CLANG_PLUS_PLUS.stderr, '\n');
+    var start_capture = false;
+
+    while (clang_plus_plus_output.next()) |line| {
+        if (STD.mem.startsWith(u8, line, "#include <...> search starts here:")) {
+            start_capture = true;
+            continue;
+        }
+
+        if (STD.mem.startsWith(u8, line, "End of search list.")) {
+            break;
+        }
+
+        if (start_capture) {
+            cflags.addIncludePath(.{ .cwd_relative = STD.mem.trim(u8, line, " ") });
+        }
+    }
 
     const CFLAGS_STEP = b.step("compile-flags", "Generate compile_flags.txt for C/C++ IDE support");
     CFLAGS_STEP.dependOn(&cflags.step);
