a2e917b53e0bc0f70fb799da9387b1429db160ba
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-11-07 00:19:45 -0400

    feat: add help documentation function
    - function prints help documentation to stdout

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const STD = @import("std");
 const CLI = @import("cli/cli.zig");
 
+pub fn help_documentation() void {
+    STD.debug.print("{s}\n", .{CLI.ASCII_ART});
+    STD.debug.print("cpplings [OPTION]\n\n", .{});
+    STD.debug.print("Options:\n", .{});
+    STD.debug.print("-h | --help | help -> output this help documentation\n", .{});
+    STD.debug.print("-s | --solutions -> run and test cpplings against solutions dir\n", .{});
+}
+
 pub fn main() !void {
     var mem_arena = STD.heap.ArenaAllocator.init(STD.heap.page_allocator);
     defer mem_arena.deinit();
