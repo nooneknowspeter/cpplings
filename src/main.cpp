da3b925c6fd2d621fb4ca9176486521175c6d59c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-24 13:29:41 -0400

    refactor(cli): update arg control flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "include/log.hpp"
 #include "include/tui.hpp"
+#include <algorithm>
 #include <print>
 #include <string>
 #include <vector>
@@ -13,9 +14,10 @@ namespace Documentation
 static const std::string MAIN{R"(
 options:
 -h | --help | help -> output this help documentation
--p | --patch [-d] -> generate patch files
--s | --solutions [-d] -> run and test cpplings against solutions dir
--d | --debug -> run cpplings in debug mode
+-d -> run cpplings in debug mode
+-p [-d] -> generate patch files
+-s [-d] -> run and test cpplings against solutions dir
+-c < zig | clang | gcc> [-d] -> run cpplings with a preferred compiler
 )"};
 
 static constexpr void printHelpDocumentation(std::string program_arg, std::string docs)
@@ -27,11 +29,9 @@ static constexpr void printHelpDocumentation(std::string program_arg, std::strin
 
 static constexpr void runCLI([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-
-    // NOTE: debug
-    Log::configureLogger(true);
-
     std::vector<std::string> args(argv, argv + argc);
+
+    Log::configureLogger(true);
 
     if (args.size() > 3)
     {
@@ -42,7 +42,14 @@ static constexpr void runCLI([[maybe_unused]] int argc, [[maybe_unused]] char *a
         return;
     }
 
-    if (args.size() == 2 && (args.at(1) == "-d" || args.at(1) == "--debug"))
+    if (std::find(args.begin(), args.end(), "-h") != args.end())
+    {
+        CLI::Documentation::printHelpDocumentation(args.at(0), CLI::Documentation::MAIN);
+
+        return;
+    }
+
+    if (std::find(args.begin(), args.end(), "-d") != args.end())
     {
         Log::configureLogger(true);
     }
@@ -56,8 +63,18 @@ static constexpr void runCLI([[maybe_unused]] int argc, [[maybe_unused]] char *a
     }
 
     // TODO: run patch system
-    if (args.at(1) == "-p")
+    if (std::find(args.begin(), args.end(), "-p") != args.end())
     {
+        Log::info("run patch system");
+
+        return;
+    }
+
+    // TODO: run tui against solutions
+    if (std::find(args.begin(), args.end(), "-s") != args.end())
+    {
+        Log::info("run tui against solutions");
+
         return;
     }
 }
