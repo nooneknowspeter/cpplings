a26e5c7752f6e86918570c5ca28f13996b0a5243
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-24 23:24:49 -0400

    refactor: update flake.nix
    - categorize packages
    - separate dev shells

--- flake.nix
+++ flake.nix
@@ -27,60 +27,97 @@
             allowUnfree = true;
           };
         };
+
+        fastfetch_cmd = "cat .ascii-art.txt | fastfetch --raw - --logo-width 15 --logo-height 7 --logo-padding-right 25 --logo-padding-top 10";
+
+        fmt_pkgs = with pkgs; [
+          treefmt
+
+          # formatters & linters
+          actionlint
+          bash-language-server
+          beautysh
+          clang-tools
+          deadnix
+          dockerfile-language-server
+          marksman
+          nil
+          nixd
+          nixfmt
+          prettier
+          shellcheck
+          statix
+          taplo
+          yaml-language-server
+          zls
+        ];
+
+        runtime_pkgs = with pkgs; [
+          # compilers & runtimes
+          clang
+          gcc
+          nodejs
+          python3
+          stdenv.cc.cc
+          zig
+        ];
+
+        dev_pkgs = with pkgs; [
+          # dev tools
+          diffutils
+          fastfetch
+          gdb
+          gnumake
+          hyperfine
+          just
+          lldb
+          patch
+          perf
+          valgrind
+					opencode
+        ];
+
+        doc_pkgs = with pkgs; [
+          # docs
+          pandoc
+          quarto
+        ];
+
+        ld_pkgs = with pkgs; [
+          stdenv.cc.cc.lib
+        ];
       in
       {
         devShells = {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              # formatters & linters
-              actionlint
-              bash-language-server
-              beautysh
-              clang-tools
-              deadnix
-              dockerfile-language-server
-              marksman
-              nil
-              nixd
-              nixfmt
-              prettier
-              shellcheck
-              statix
-              taplo
-              yaml-language-server
-              zls
-
-              # compilers & runtimes
-              clang
-              gcc
-              nodejs
-              python3
-              stdenv.cc.cc
-              zig
-
-              # dev tools
-              fastfetch
-              gdb
-              gnumake
-              hyperfine
-              just
-              lldb
-              perf
-              treefmt
-              valgrind
-
-              # docs
-              pandoc
-              quarto
-            ];
+            packages = [
+            ]
+            ++ fmt_pkgs
+            ++ runtime_pkgs
+            ++ dev_pkgs
+            ++ doc_pkgs;
 
             shellHook = ''
-              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-              cat .ascii-art.txt | fastfetch --raw - --logo-width 15 --logo-height 7 --logo-padding-right 25 --logo-padding-top 10
+              ${fastfetch_cmd}
             '';
 
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath ld_pkgs}:$LD_LIBRARY_PATH";
+          };
+
+          fmt = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                just
+              ]
+              ++ fmt_pkgs;
+
+            shellHook = ''
+              ${fastfetch_cmd}
+            '';
           };
         };
       }
     );
 }
+
