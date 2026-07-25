08f9ab54a574b05f773d126227240ee1c60d0e5c
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-25 14:19:29 -0400

    refactor(watcher): sync thread
    - lock critical sections
    - notify consumers through cv; runner
    - heap allocate and return pointer on get instance
    - use state bool as loop condition

--- src/file_watcher.cpp
+++ src/file_watcher.cpp
@@ -4,30 +4,37 @@
 #include <chrono>
 #include <filesystem>
 #include <memory>
+#include <mutex>
 #include <thread>
 
-FileWatcher &FileWatcher::getInstance()
+std::unique_ptr<FileWatcher> FileWatcher::getInstance()
 {
-    static FileWatcher instance;
-
-    return instance;
+    return std::make_unique<FileWatcher>();
 }
 
-void watch(const std::uint16_t poll_ms)
+void FileWatcher::watch(const std::uint16_t poll_ms)
 {
-    while (true)
+    while (TUI::p_state->is_running)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(poll_ms));
 
-        if (std::filesystem::exists(TUI::p_state->current_exercise))
+        std::filesystem::path current_exercise;
         {
+            std::scoped_lock lock(TUI::mutex_state);
+            current_exercise = TUI::p_state->current_exercise;
+        }
 
-            [[maybe_unused]] auto file_time{std::filesystem::last_write_time(TUI::p_state->current_exercise)};
+        if (std::filesystem::exists(current_exercise))
+        {
+            auto file_time{std::filesystem::last_write_time(current_exercise)};
 
             if (this->m_current_exercise_prev_mod_time != file_time)
             {
                 Log::info("file modified, refreshing...");
+                std::scoped_lock lock(TUI::mutex_state);
                 TUI::p_commands_queue->push(TUI::Commands::Refresh);
+
+                TUI::cv_state.notify_one();
             }
 
             this->m_current_exercise_prev_mod_time = file_time;
--- src/include/file_watcher.hpp
+++ src/include/file_watcher.hpp
@@ -14,11 +14,10 @@ struct FileWatcher
     FileWatcher &operator=(const FileWatcher &) = delete;
     FileWatcher &operator=(FileWatcher &&) = default;
 
-    static FileWatcher &getInstance();
+    static std::unique_ptr<FileWatcher> getInstance();
 
     void watch(const std::uint16_t poll_ms = 1000);
 
   private:
-
     [[maybe_unused]] std::filesystem::file_time_type m_current_exercise_prev_mod_time;
 };
